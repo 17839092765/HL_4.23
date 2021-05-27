9bf34e6da02556797892855210a872cd0bd3033b
张祥 <1341307180@qq.com>
2021-05-27 18:35:53 +0800

    5.27

--- public/static/ac_conf.js
+++ public/static/ac_conf.js
@@ -1,12 +1,7 @@
-// const CASEIP='10.162.177.79'
-// const CASEIP='10.166.6.227'
-// const CASEIP='192.168.5.124'
-const CASEIP = "192.168.5.112";
-//49服务器的服务
 var HostConfig = {
-  MatchServer: CASEIP + ":81",
-  AirCityPlayer: CASEIP + ":8081",
-  AirCityAPI: CASEIP + ":4323",
-  Token: "",
-  Path: "C:\\Users\\张祥\\AppData\\Roaming\\AirCityCloud\\SDK",
-};
+  "MatchServer": "192.168.5.112:81",
+  "AirCityPlayer": "192.168.5.112:8082",
+  "AirCityAPI": "192.168.5.112:4324",
+  "Token": "",
+  "Path": "C:\\Users\\张祥\\AppData\\Roaming\\AirCityCloud\\SDK"
+}
